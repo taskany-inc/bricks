66ace802bed32a67a3cb6f3e37a34a210fd98343
Tony Vi <winogradovaa@gmail.com>
2023-06-09 18:18:58 +0300

    feat(FormTextrea): support minHeight as prop

--- src/components/FormTextarea.tsx
+++ src/components/FormTextarea.tsx
@@ -15,6 +15,7 @@ interface FormTextareaProps {
     placeholder?: string;
     disabled?: boolean;
     flat?: 'top' | 'bottom' | 'both';
+    minHeight?: number;
     error?: {
         message?: string;
     };
@@ -26,10 +27,14 @@ interface FormTextareaProps {
 }
 
 const StyledFormTextarea = styled(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    ({ flat, forwardRef, ...props }: FormTextareaProps & { forwardRef?: React.Ref<HTMLTextAreaElement> }) => (
-        <textarea ref={forwardRef} {...props} />
-    ),
+    ({
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        flat,
+        forwardRef,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        minHeight,
+        ...props
+    }: FormTextareaProps & { forwardRef?: React.Ref<HTMLTextAreaElement> }) => <textarea ref={forwardRef} {...props} />,
 )`
     box-sizing: border-box;
     outline: none;
@@ -41,7 +46,7 @@ const StyledFormTextarea = styled(
     font-size: 16px;
     padding: 8px 16px;
     width: 100%;
-    min-height: 200px;
+    min-height: ${({ minHeight = 200 }) => `${minHeight}px`};
     resize: none;
 
     transition: 200ms cubic-bezier(0.3, 0, 0.5, 1);
