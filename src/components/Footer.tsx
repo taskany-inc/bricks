b4b37d15f72f2e34253d2bcc5e2c388d36190612
troff8 <ISeTrofimov@sberbank.ru>
2023-07-05 22:20:50 +0400

    fix(Footer): pass ready jsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { gray0, gray9 } from '@taskany/colors';
+import { gray0 } from '@taskany/colors';
 
 import { SheepLogo } from './SheepLogo';
 import Text from './Text';
-import Link from './Link';
 
 const StyledFooter = styled.footer`
     display: grid;
@@ -17,29 +16,19 @@ const StyledFooterMenu = styled.div`
     display: flex;
 `;
 
-const StyledText = styled(Text)`
+export const FooterItem = styled(Text)`
     padding: 0px 10px;
 `;
-
 interface FooterProps {
     className?: string;
-    menuItems: {
-        title: string;
-        url: string;
-    }[];
+    children: React.ReactNode;
 }
 
-export const Footer: React.FC<FooterProps> = ({ className, menuItems }) => {
+export const Footer: React.FC<FooterProps> = ({ className, children }) => {
     return (
         <StyledFooter className={className}>
             <Text color={gray0}>{`© ${new Date().getFullYear()} Taskany, Inc.`}</Text>
-            <StyledFooterMenu>
-                {menuItems.map(({ title, url }) => (
-                    <Link key={url} href={url} inline>
-                        <StyledText color={gray9}>{title}</StyledText>
-                    </Link>
-                ))}
-            </StyledFooterMenu>
+            <StyledFooterMenu>{children}</StyledFooterMenu>
             <SheepLogo />
         </StyledFooter>
     );
