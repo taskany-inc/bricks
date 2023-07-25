301515acac220a38ce1b1bd547b80097ffb910d6
troff8 <ISeTrofimov@sberbank.ru>
2023-07-25 17:34:28 +0400

    feat(Card,CardContent): remove specific styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import { gray4, radiusM } from '@taskany/colors';
 export const Card = styled.div`
     position: relative;
     box-sizing: border-box;
-    min-height: 150px;
+    min-height: auto;
 
     border: 1px solid ${gray4};
     border-radius: ${radiusM};
--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const CardContent = styled.div`
     position: relative;
     box-sizing: border-box;
-    padding: 12px 14px 60px;
+    padding: 12px 14px;
 `;
 
 export default CardContent;
