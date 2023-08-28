f2857aab8636dacfe31ca85111cec0d86706fb0b
Tony Vi <winogradovaa@gmail.com>
2023-08-29 01:59:40 +0200

    feat(nullable): support array types

--- src/utils/nullable.ts
+++ src/utils/nullable.ts
@@ -2,5 +2,9 @@ import React from 'react';
 
 // eslint-disable-next-line no-shadow
 export function nullable<V>(v: V, render: (v: NonNullable<V>) => React.ReactNode) {
-    return v ? render(v as NonNullable<V>) : null;
+    if (!v) return null;
+
+    if (Array.isArray(v) && !v.length) return null;
+
+    return render(v as NonNullable<V>);
 }
