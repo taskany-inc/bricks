a3f42b01da3aef81ab7e5039d4fadee170875ab8
Tony Vi <winogradovaa@gmail.com>
2023-09-07 18:53:48 +0000

    refactor(Form): remove redundant style

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { gray3 } from '@taskany/colors';
 
 import { KeyCode, KeyMod, useKeyboard } from '../hooks/useKeyboard';
 import { formContext } from '../context/form';
@@ -9,16 +8,14 @@ interface FormProps extends React.HTMLAttributes<HTMLFormElement> {
     children: React.ReactNode;
     disabled?: boolean;
     submitHotkey?: Array<number>;
+    className?: string;
 
     onSubmit?: () => void;
 }
 
 const submitKeys = [KeyMod.CtrlCmd, KeyCode.Enter];
 
-// TODO: https://github.com/taskany-inc/bricks/issues/448
-const StyledForm = styled.form`
-    background-color: ${gray3};
-`;
+const StyledForm = styled.form``;
 
 export const Form: React.FC<FormProps> = ({ children, disabled, submitHotkey = submitKeys, onSubmit, ...attrs }) => {
     const handleSubmit = (e?: React.SyntheticEvent) => {
