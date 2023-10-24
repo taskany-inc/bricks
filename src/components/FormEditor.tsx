2e5fda8072207b20ab8512fa926ba10a6a9874b7
Anton <sab94a@yandex.ru>
2023-10-24 20:19:24 +0300

    feat: monaco cdn path to env

--- src/components/FormEditor.tsx
+++ src/components/FormEditor.tsx
@@ -2,7 +2,7 @@
 import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { useDropzone } from 'react-dropzone';
-import Editor from '@monaco-editor/react';
+import Editor, { loader } from '@monaco-editor/react';
 import { danger10, gapS, gapXs, gray2, gray3, gray4, gray6, gray8, radiusS, textColor } from '@taskany/colors';
 import { IconAttachOutline } from '@taskany/icons';
 
@@ -14,6 +14,12 @@ import { formContext } from '../context/form';
 
 import { Popup } from './Popup/Popup';
 import { Link } from './Link/Link';
+
+loader.config({
+    paths: {
+        vs: process.env.NEXT_PUBLIC_MONACO_CDN || 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs',
+    },
+});
 
 interface FormEditorProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange' | 'onBlur' | 'onFocus'> {
     id?: string;
