ecf740738623ec32a50cac2bad59f3447d198fbe
Tony Vi <winogradovaa@gmail.com>
2023-10-31 22:23:18 +0300

    chore(Link): rewrite to CSS modules

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import { link10 } from '@taskany/colors';
+import cn from 'classnames';
+
+import styles from './Link.module.css';
 
 interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
     inline?: boolean;
@@ -10,47 +11,19 @@ interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
     target?: '_self' | '_blank' | '_parent' | '_top';
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     as?: string | React.ComponentType<any>;
-
-    onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
-const StyledLink = styled(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    ({ forwardRef, inline, ...props }: LinkProps & { forwardRef?: React.Ref<HTMLAnchorElement> }) => (
-        <a ref={forwardRef} {...props} />
-    ),
-)`
-    color: ${link10};
+export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
+    ({ inline, className, as: Tag = 'a', ...props }, ref) => {
+        const cls = cn(styles.Link, { [styles.LinkInline]: inline }, className);
 
-    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
-    transition-property: color;
+        const onClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+            if (e.metaKey || e.ctrlKey || !props.onClick) return;
 
-    cursor: pointer;
+            e.preventDefault();
+            props.onClick(e);
+        };
 
-    &:hover,
-    &:focus {
-        transition-duration: 0.1s;
-    }
-
-    ${({ inline }) =>
-        inline &&
-        `
-            color: inherit;
-            text-decoration: none;
-
-            &:hover {
-                color: ${link10};
-            }
-        `}
-`;
-
-export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(({ ...props }, ref) => {
-    const onClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        if (e.metaKey || e.ctrlKey || !props.onClick) return;
-
-        e.preventDefault();
-        props.onClick(e);
-    };
-
-    return <StyledLink {...props} onClick={onClick} forwardRef={ref} />;
-});
+        return <Tag ref={ref} className={cls} {...props} onClick={onClick} />;
+    },
+);
