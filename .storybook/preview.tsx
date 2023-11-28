c9946ac825a9dd47ec99f9b83a270d10804496a1
DenisVorop <saskeazaske@icloud.com>
2023-11-28 13:15:31 +0300

    feat(Storybook): add theme support

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import type { Preview } from '@storybook/react';
-import { LightTheme } from '@taskany/colors';
+import React, { useEffect } from 'react';
+import type { Decorator, Preview } from '@storybook/react';
+import { DarkTheme, LightTheme, backgroundColor } from '@taskany/colors';
+import styled from 'styled-components';
+
 import '@taskany/colors/harmony/dark.css';
 import '@taskany/colors/harmony/light.css';
 
 import { TextStyle } from '../src/components/Text/Text';
+
+const ThemeBlock = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    padding: 1rem;
+    background: ${backgroundColor};
+`
+
+const withTheme: Decorator = (StoryFn, context) => {
+    const { theme } = context.globals
+    const Theme = theme === 'dark' ? DarkTheme : LightTheme
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme)
+    }, [theme])
+
+    return (
+        <>
+            <Theme />
+            <TextStyle />
+            <ThemeBlock theme={theme}>
+                <StoryFn />
+            </ThemeBlock>
+        </>
+    )
+}
 
 const preview: Preview = {
     parameters: {
@@ -17,15 +49,23 @@ const preview: Preview = {
         },
         layout: 'centered',
     },
-    decorators: [
-        (Story) => (
-            <>
-                <LightTheme/>
-                <TextStyle />
-                <Story />
-            </>
-        )
-    ],
+    decorators: [withTheme],
 };
 
 export default preview;
+
+export const globalTypes = {
+    theme: {
+        name: 'Theme',
+        description: 'Global theme for components',
+        defaultValue: 'light',
+        toolbar: {
+            icon: 'circlehollow',
+            items: [
+            { value: 'light', icon: 'circlehollow', title: 'light' },
+            { value: 'dark', icon: 'circle', title: 'dark' },
+            ],
+            showName: true,
+        },
+    },
+}
