39dbaeb1d1038eff810656d392007511202f7551
DenisVorop <saskeazaske@icloud.com>
2023-11-30 14:58:39 +0300

    chore: lint

--- .storybook/external/icons.stories.tsx
+++ .storybook/external/icons.stories.tsx
@@ -1,33 +1,32 @@
 import React, { useCallback, useDeferredValue, useEffect, useMemo, useRef, useState } from 'react';
-import { Meta, StoryFn } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react';
 import * as Icons from '@taskany/icons';
 import type { BaseIconProps } from '@taskany/icons';
 import styled from 'styled-components';
 
 import { Text } from '../../src/components/Text/Text';
 import { Input } from '../../src/components/Input/Input';
-import { Popup } from '../../src/components/Popup/Popup'
+import { Popup } from '../../src/components/Popup/Popup';
 
-type IconStory = React.ComponentType<{ type: 'solid' | 'outline' } & Omit<BaseIconProps, 'value'>>
+type IconStory = React.ComponentType<{ type: 'solid' | 'outline' } & Omit<BaseIconProps, 'value'>>;
 
 export default {
     title: '@taskany/icons',
     argTypes: {
         size: {
-            options: ["xxs", "xs", "s", "m", "l"],
+            options: ['xxs', 'xs', 's', 'm', 'l'],
             control: { type: 'inline-radio' },
         },
         type: {
             options: ['solid', 'outline'],
             control: { type: 'inline-radio' },
-
         },
         color: {
             type: 'string',
         },
         noWrap: {
             type: 'boolean',
-        }
+        },
     },
     args: {
         size: 's',
@@ -36,25 +35,25 @@ export default {
     },
     parameters: {
         layout: 'padded',
-    }
+    },
 } as Meta<IconStory>;
 
 type RenderIconProps = {
     name: string;
     originalName: string;
-    icon: React.ComponentType<Omit<React.ComponentProps<typeof Icons.BaseIcon>, 'value'>>
-}
+    icon: React.ComponentType<Omit<React.ComponentProps<typeof Icons.BaseIcon>, 'value'>>;
+};
 
-const regex = /(?:(?:([a-z][A-Z]?\d?)([A-Z]))|(?:([a-z])(\d))|(?:(\d)([A-Z])))/g
+const regex = /(?:(?:([a-z][A-Z]?\d?)([A-Z]))|(?:([a-z])(\d))|(?:(\d)([A-Z])))/g;
 
-const splitName = (str: string) => str.replace(regex, (substring) => substring.split('').join(' '))
+const splitName = (str: string) => str.replace(regex, (substring) => substring.split('').join(' '));
 
 const pickIconName = (name: string) => {
     const normalizeName = splitName(name);
     const splittedName = normalizeName.split(' ');
 
     return splittedName.slice(1, splittedName.length - 1).join(' ');
-}
+};
 
 const getIcons = (type: 'solid' | 'outline') => {
     return Object.keys(Icons).reduce<RenderIconProps[]>((acc, iconName) => {
@@ -70,12 +69,12 @@ const getIcons = (type: 'solid' | 'outline') => {
 
         return acc;
     }, []);
-}
+};
 
 const Wrapper = styled.div`
     max-width: 500px;
     margin: 0 auto;
-`
+`;
 
 const Grid = styled.div`
     display: grid;
@@ -115,13 +114,13 @@ const Item: React.FC<
         if (showTooltip) {
             timerRef.current = window.setTimeout(() => {
                 setShowTolltip(false);
-            }, 1500)
+            }, 1500);
         }
 
         return () => {
             window.clearTimeout(timerRef.current);
-        }
-    }, [showTooltip])
+        };
+    }, [showTooltip]);
 
     const handleClick = useCallback(() => {
         copyToClipBoard(`import { ${originalName} } from '@taskany/icons';`);
@@ -134,7 +133,7 @@ const Item: React.FC<
             window.clearTimeout(timerRef.current);
             setShowTolltip(false);
         }
-    }, [showTooltip])
+    }, [showTooltip]);
 
     return (
         <>
@@ -151,16 +150,18 @@ const Item: React.FC<
             </Popup>
 
             <Popup visible={showTooltip} reference={nodeRef} placement="top">
-                <Text size="xs" onClick={hide}>Copied!</Text>
+                <Text size="xs" onClick={hide}>
+                    Copied!
+                </Text>
             </Popup>
         </>
     );
-}
+};
 
 const Results: React.FC<{
     type: 'solid' | 'outline';
     searchValue: string;
-    size: "xxs" | "xs" | "s" | "m" | "l" | number;
+    size: 'xxs' | 'xs' | 's' | 'm' | 'l' | number;
     color?: string;
 }> = ({ type, searchValue = '', size, color }) => {
     const iconsToRender = useMemo(() => getIcons(type), [type]);
@@ -168,7 +169,11 @@ const Results: React.FC<{
 
     return (
         <>
-            {!filteredList.length && <Text size="s" color="gray">Nothing found</Text>}
+            {!filteredList.length && (
+                <Text size="s" color="gray">
+                    Nothing found
+                </Text>
+            )}
             {filteredList.length && (
                 <Grid>
                     {filteredList.map(({ name, icon: Icon, originalName }) => (
@@ -179,8 +184,8 @@ const Results: React.FC<{
                 </Grid>
             )}
         </>
-    )
-}
+    );
+};
 
 export const All: StoryFn<IconStory> = (args) => {
     const [search, setSearch] = useState('');
@@ -191,12 +196,12 @@ export const All: StoryFn<IconStory> = (args) => {
             <InputWrapper>
                 <Input
                     value={search}
-                    placeholder='Type to search icon ...'
+                    placeholder="Type to search icon ..."
                     onChange={(ev) => setSearch(ev.target.value)}
                     style={{ width: '300px', margin: '0 auto' }}
                 />
             </InputWrapper>
             <Results type={args.type} size={args.size} color={args.color} searchValue={deferredValue} />
         </Wrapper>
-    )
-}
+    );
+};
