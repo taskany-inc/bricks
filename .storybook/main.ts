f4a09573f52e292b8703e1b6fdf79988f28c1529
DenisVorop <saskeazaske@icloud.com>
2023-11-30 14:58:39 +0300

    chore: lint

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)', './external/**/*.mdx', './external/**/*.stories.tsx'],
+    stories: [
+        '../src/**/*.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+        './external/**/*.mdx',
+        './external/**/*.stories.tsx',
+    ],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: {
         name: '@storybook/react-webpack5',
