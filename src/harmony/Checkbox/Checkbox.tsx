1f673542fad284ba60bea50758751d3c88cfc166
Maksim Sviridov <enemyworld@gmail.com>
2023-12-28 19:46:54 +0300

    fix(Checkbox,Radio): onChange handler

--- src/harmony/Checkbox/Checkbox.tsx
+++ src/harmony/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useCallback, useState } from 'react';
+import React, { forwardRef } from 'react';
 import cn from 'classnames';
 
 import { nullable } from '../../utils';
@@ -11,35 +11,22 @@ interface CheckboxProps extends React.HTMLAttributes<HTMLInputElement> {
     onChange: React.ChangeEventHandler<HTMLInputElement>;
     label?: React.ReactNode;
     name: string;
+    value?: string;
 }
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-    ({ checked, onChange, label, name, className, ...rest }, ref) => {
-        const [innerState, setInnerState] = useState(() => checked);
-
-        const onChangeHandler = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
-            (event) => {
-                setInnerState((prev) => !prev);
-
-                onChange(event);
-            },
-            [onChange],
-        );
-
-        return (
-            <label className={cn(classes.Checkbox, className)}>
-                <input
-                    className={cn(classes.CheckboxInput)}
-                    type="checkbox"
-                    name={name}
-                    defaultChecked={checked}
-                    checked={innerState}
-                    onChange={onChangeHandler}
-                    ref={ref}
-                    {...rest}
-                />
-                {nullable(label, (labelComponent) => labelComponent)}
-            </label>
-        );
-    },
+    ({ checked, onChange, label, name, className, ...rest }, ref) => (
+        <label className={cn(classes.Checkbox, className)}>
+            <input
+                className={cn(classes.CheckboxInput)}
+                type="checkbox"
+                name={name}
+                defaultChecked={checked}
+                onChange={onChange}
+                ref={ref}
+                {...rest}
+            />
+            {nullable(label, (labelComponent) => labelComponent)}
+        </label>
+    ),
 );
