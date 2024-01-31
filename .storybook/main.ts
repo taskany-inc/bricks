737419cf6ef0e7240f8a21a8b68060d12f9a6b1c
Maksim Sviridov <enemyworld@gmail.com>
2024-01-31 13:48:35 +0300

    build(sb): migrate build from webpack to vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-vite';
+import { defineConfig } from 'vite';
 import { resolve } from 'node:path/posix';
 
 const config: StorybookConfig = {
@@ -9,88 +10,34 @@ const config: StorybookConfig = {
         './external/**/*.stories.tsx',
     ],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    core: {
+        builder: '@storybook/builder-vite',
+    },
     framework: {
-        name: '@storybook/react-webpack5',
+        name: '@storybook/react-vite',
         options: {},
     },
     docs: {
         autodocs: 'tag',
     },
-    previewHead: (head) => `
-        ${head}
-        <link rel="stylesheet" href="public/dark.css" id="harmony_theme"></link>
-    `,
     staticDirs: ['./public'],
-    webpackFinal: async (config) => {
-        config.resolve = {
-            ...config.resolve,
-            alias: {
-                ...config.resolve?.alias,
-                react: resolve(process.cwd(), 'node_modules', 'react'),
-                'react-dom': resolve(process.cwd(), 'node_modules', 'react-dom'),
-                '@taskany': resolve(process.cwd(), 'node_modules', '@taskany'),
-            },
-        };
-
-        const { rules = [] } = config.module || {};
-
-        const styleRuleIndex = rules.findIndex((rule) => {
-            if (rule != null && typeof rule === 'object') {
-                if (rule.test instanceof RegExp) {
-                    return /\.css$/.source === rule.test.source;
-                }
-            }
-
-            return false;
-        });
-
-        if (styleRuleIndex > -1) {
-            rules.splice(
-                styleRuleIndex,
-                1,
-                {
-                    test: /\.css$/,
-                    exclude: /harmony\/(light|dark)\.css$/,
-                    sideEffects: true,
-                    use: [
-                        'style-loader',
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1,
-                            },
-                        },
-                    ],
+    viteFinal: (config, { configType }) => {
+        if (configType === 'DEVELOPMENT') {
+            config.resolve = {
+                ...config.resolve,
+                alias: {
+                    ...config.resolve?.alias,
+                    react: resolve(process.cwd(), 'node_modules', 'react'),
+                    'react-dom': resolve(process.cwd(), 'node_modules', 'react-dom'),
+                    '@taskany': resolve(process.cwd(), 'node_modules', '@taskany'),
                 },
-                {
-                    test: /harmony\/(light|dark)\.css$/,
-                    sideEffects: true,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                injectType: 'linkTag',
-                                attributes: {
-                                    id: 'harmony',
-                                },
-                                esModule: false,
-                                insert: () => {},
-                            },
-                        },
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]',
-                                outputPath: 'public',
-                                esModule: false,
-                            },
-                        },
-                    ],
-                },
-            );
+            };
         }
 
-        return config;
+        return defineConfig({
+            ...config,
+            assetsInclude: ['/sb-preview/runtime.js', '/dark.css', '/light.css'],
+        });
     },
 };
 export default config;
