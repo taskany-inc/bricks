87f6a61b99bc77a130afdb184641dbdbd967e663
Anton <sab94a@yandex.ru>
2024-02-02 16:57:32 +0300

    fix: harmony User gravatar fallback

--- src/harmony/User/User.tsx
+++ src/harmony/User/User.tsx
@@ -5,6 +5,7 @@ import { Circle } from '../Circle/Circle';
 import { preloadImage } from '../../utils/preloadImage';
 import { nullable } from '../../utils';
 import { getInitials } from '../../utils/getInitials';
+import { Gravatar } from '../../components';
 
 import classes from './User.module.css';
 
@@ -19,27 +20,35 @@ interface UserProps {
 
 type AvatarLoadState = 'success' | 'error' | 'loading';
 
+const Avatar: React.FC<Pick<UserProps, 'name' | 'email' | 'src'>> = ({ src, email, name }) => {
+    const [status, setStatus] = useState<AvatarLoadState>('loading');
+
+    if (!src || status === 'error') {
+        return <Gravatar name={name} email={email} size={24} />;
+    }
+
+    preloadImage(src)
+        .then(() => setStatus('success'))
+        .catch(() => setStatus('error'));
+
+    return (
+        <Circle string={`${email}`} className={cn(classes.UserPicWrapper)}>
+            {nullable(
+                status === 'success',
+                () => (
+                    <img src={src} alt={name || email} className={classes.UserImage} />
+                ),
+                getInitials(name),
+            )}
+        </Circle>
+    );
+};
+
 export const User = forwardRef<HTMLDivElement, UserProps>(
     ({ email, name, src, short, inheritColor, className }, ref) => {
-        const [status, setStatus] = useState<AvatarLoadState>('loading');
-
-        if (src) {
-            preloadImage(src)
-                .then(() => setStatus('success'))
-                .catch(() => setStatus('error'));
-        }
-
         return (
             <div className={cn(classes.User, className)} ref={ref}>
-                <Circle string={`${email}`} className={cn(classes.UserPicWrapper)}>
-                    {nullable(
-                        status === 'success',
-                        () => (
-                            <img src={src} alt={name || email} className={classes.UserImage} />
-                        ),
-                        getInitials(name),
-                    )}
-                </Circle>
+                <Avatar src={src} name={name} email={email} />
                 {nullable(!short, () => (
                     <span
                         className={cn(classes.UserName, {
