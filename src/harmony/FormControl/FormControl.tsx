a1ad0327568b216783bbf3888cbfc1d80c7e8cce
DenisVorop <saskeazaske@icloud.com>
2024-02-07 21:42:57 +0300

    feat(Harmony): add FormControl component

---
+++ src/harmony/FormControl/FormControl.tsx
@@ -0,0 +1,119 @@
+import React, {
+    ComponentProps,
+    HTMLAttributes,
+    MutableRefObject,
+    PropsWithChildren,
+    ReactNode,
+    createContext,
+    forwardRef,
+    useCallback,
+    useContext,
+    useEffect,
+    useId,
+    useMemo,
+    useRef,
+    useState,
+} from 'react';
+import { IconExclamationCircleSolid } from '@taskany/icons';
+
+import { Input } from '../Input/Input';
+import { Text } from '../Text/Text';
+import { nullable } from '../../utils';
+import { Tooltip } from '../Tooltip/Tooltip';
+
+interface FormControlContext {
+    id?: string;
+    error?: {
+        message?: ReactNode;
+    };
+    popupRef: MutableRefObject<HTMLDivElement | null>;
+    handleError: (error: { message?: ReactNode } | undefined) => void;
+}
+
+const FormControlContext = createContext<FormControlContext | void>(undefined);
+
+const useFormControlContext = () => {
+    const formContext = useContext(FormControlContext);
+
+    if (!formContext) {
+        throw new Error("Don't use before initialization or outside of `FormControl` component");
+    }
+
+    return formContext;
+};
+
+interface FormControlProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {}
+
+export const FormControl = ({ children, ...props }: FormControlProps) => {
+    const [error, setError] = useState<{ message?: ReactNode } | undefined>();
+    const popupRef = useRef<HTMLDivElement | null>(null);
+    const id = useId();
+
+    const handleError = useCallback((error: { message?: ReactNode } | undefined) => {
+        setError(error);
+    }, []);
+
+    const ctx = useMemo(() => ({ id, error, popupRef, handleError }), [id, error, popupRef, handleError]);
+
+    return (
+        <FormControlContext.Provider value={ctx}>
+            <div {...props}>{children}</div>
+        </FormControlContext.Provider>
+    );
+};
+
+interface FormControlInputProps extends ComponentProps<typeof Input> {}
+
+export const FormControlInput = forwardRef<HTMLInputElement, FormControlInputProps>(({ size = 's', ...props }, ref) => {
+    const { id, error, popupRef } = useFormControlContext();
+
+    return (
+        <Input
+            id={id}
+            view={error ? 'danger' : 'default'}
+            ref={ref}
+            size={size}
+            forwardedRef={popupRef}
+            iconRight={error && <IconExclamationCircleSolid size={size === 'm' ? 's' : size} />}
+            pointerEvents="none"
+            {...props}
+        />
+    );
+});
+
+interface FormControlLabelProps extends Omit<ComponentProps<typeof Text<'label'>>, 'as'> {}
+
+export const FormControlLabel = ({ ...props }: FormControlLabelProps) => {
+    const { id } = useFormControlContext();
+    return <Text htmlFor={id} as="label" {...props} />;
+};
+
+interface FormControlErrorProps extends ComponentProps<typeof Tooltip> {
+    error?: NonNullable<FormControlContext['error']>;
+}
+
+export const FormControlError = ({ children, error, placement = 'bottom', ...props }: FormControlErrorProps) => {
+    const { popupRef, handleError } = useFormControlContext();
+
+    useEffect(() => {
+        if (error) {
+            handleError(error);
+        } else {
+            handleError({ message: children });
+        }
+
+        return () => {
+            handleError(undefined);
+        };
+    }, [children, error, handleError]);
+
+    if (error && children) {
+        throw new Error("FormControlError: Both 'error' and 'children' props are provided. Only one should be used.");
+    }
+
+    return (
+        <Tooltip view="danger" reference={popupRef} interactive placement={placement} {...props}>
+            {nullable(error, ({ message }) => message, children)}
+        </Tooltip>
+    );
+};
