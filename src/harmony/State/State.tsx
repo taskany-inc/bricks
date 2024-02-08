a2800be19250cd6b6d8a30556b6f084271377c44
Tony Vi <winogradovaa@gmail.com>
2024-02-08 15:24:38 +0300

    fix(State): pass html attrs

--- src/harmony/State/State.tsx
+++ src/harmony/State/State.tsx
@@ -5,13 +5,19 @@ import { Badge } from '../Badge/Badge';
 
 import s from './State.module.css';
 
-interface StateProps {
+interface StateProps extends React.HTMLAttributes<HTMLSpanElement> {
     title?: string;
     color?: string;
 }
 
-export const State = React.forwardRef<HTMLSpanElement, StateProps>(({ title, color }, ref) => {
+export const State = React.forwardRef<HTMLSpanElement, StateProps>(({ title, color, ...attrs }, ref) => {
     return (
-        <Badge ref={ref} iconLeft={<Dot size="l" className={s.StateDot} color={color} />} text={title} color={color} />
+        <Badge
+            ref={ref}
+            iconLeft={<Dot size="l" className={s.StateDot} color={color} />}
+            text={title}
+            color={color}
+            {...attrs}
+        />
     );
 });
