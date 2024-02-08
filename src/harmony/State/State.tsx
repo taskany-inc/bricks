87f5097baf33f1448e5e8911c027b536f011cf0a
Tony Vi <winogradovaa@gmail.com>
2024-02-08 15:18:27 +0300

    chore(Badge,State): ref forwarding

--- src/harmony/State/State.tsx
+++ src/harmony/State/State.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { Dot } from '../Dot/Dot';
 import { Badge } from '../Badge/Badge';
 
-import classes from './State.module.css';
+import s from './State.module.css';
 
 interface StateProps {
     title?: string;
     color?: string;
 }
 
-export const State: React.FC<StateProps> = ({ title, color }) => {
-    return <Badge iconLeft={<Dot size="l" className={classes.StateDot} color={color} />} text={title} color={color} />;
-};
+export const State = React.forwardRef<HTMLSpanElement, StateProps>(({ title, color }, ref) => {
+    return (
+        <Badge ref={ref} iconLeft={<Dot size="l" className={s.StateDot} color={color} />} text={title} color={color} />
+    );
+});
