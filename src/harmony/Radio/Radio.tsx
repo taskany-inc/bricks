a8832370d2c0646ef0c4f00c7b73017e1894eb7d
Maksim Sviridov <enemyworld@gmail.com>
2024-03-01 20:11:24 +0300

    fix(Radio): pass `readOnly` prop

--- src/harmony/Radio/Radio.tsx
+++ src/harmony/Radio/Radio.tsx
@@ -9,24 +9,16 @@ interface RadioProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: React.ReactNode;
 }
 
-export const Radio = forwardRef<HTMLInputElement, RadioProps>(
-    ({ label, className, disabled, checked, ...rest }, ref) => (
-        <label
-            className={cn(classes.Radio, className, {
-                [classes.RadioDisabled]: disabled,
-            })}
-        >
-            <input
-                className={cn(classes.RadioInput)}
-                type="radio"
-                ref={ref}
-                disabled={disabled}
-                {...rest}
-                defaultChecked={checked}
-            />
-            {nullable(label, (labelComponent) => (
-                <span className={cn(classes.RadioLabel)}>{labelComponent}</span>
-            ))}
-        </label>
-    ),
-);
+export const Radio = forwardRef<HTMLInputElement, RadioProps>(({ label, className, checked, ...rest }, ref) => (
+    <label
+        className={cn(classes.Radio, className, {
+            [classes.RadioDisabled]: rest.disabled,
+            [classes.RadioReadonly]: rest.readOnly,
+        })}
+    >
+        <input className={cn(classes.RadioInput)} type="radio" ref={ref} {...rest} defaultChecked={checked} />
+        {nullable(label, (labelComponent) => (
+            <span className={cn(classes.RadioLabel)}>{labelComponent}</span>
+        ))}
+    </label>
+));
