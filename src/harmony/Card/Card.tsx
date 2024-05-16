ed5bf395b9db78b996546d55ccc6395285f9d823
Anton Sabotovich <sab94a@yandex.ru>
2024-05-16 11:52:52 +0300

    feat: support border-color

--- src/harmony/Card/Card.tsx
+++ src/harmony/Card/Card.tsx
@@ -3,35 +3,31 @@ import cn from 'classnames';
 
 import s from './Card.module.css';
 
-export const Card: FC<HTMLAttributes<HTMLDivElement>> = ({ className, children, ...props }) => {
-    return (
-        <div className={cn(s.Card, className)} {...props}>
-            {children}
-        </div>
-    );
-};
-
-interface CardInfoProps extends HTMLAttributes<HTMLDivElement> {
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
     backgroundColor?: string;
     foregroundColor?: string;
-    corner?: boolean;
 }
 
-export const CardInfo: FC<CardInfoProps> = ({
-    className,
-    children,
-    foregroundColor,
-    backgroundColor,
-    corner,
-    ...props
-}) => {
+export const Card: FC<CardProps> = ({ className, children, backgroundColor, foregroundColor, ...props }) => {
     const style = {
         '--card-background': backgroundColor,
         '--card-foreground': foregroundColor,
     } as React.CSSProperties;
 
     return (
-        <div className={cn(s.CardInfo, { [s.CardInfo_corner]: corner }, className)} style={style} {...props}>
+        <div className={cn(s.Card, className)} style={style} {...props}>
+            {children}
+        </div>
+    );
+};
+
+interface CardInfoProps extends HTMLAttributes<HTMLDivElement> {
+    corner?: boolean;
+}
+
+export const CardInfo: FC<CardInfoProps> = ({ className, children, corner, ...props }) => {
+    return (
+        <div className={cn(s.CardInfo, { [s.CardInfo_corner]: corner }, className)} {...props}>
             {children}
         </div>
     );
