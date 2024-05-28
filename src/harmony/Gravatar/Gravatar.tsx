f50a8287409da15f63b3258dbdc7ca2b52899454
Anton Sabotovich <sab94a@yandex.ru>
2024-05-28 13:54:54 +0300

    fix: gravatar host

--- src/harmony/Gravatar/Gravatar.tsx
+++ src/harmony/Gravatar/Gravatar.tsx
@@ -29,7 +29,7 @@ export const Gravatar: FC<GravatarProps> = ({
     size = 's',
     rating = 'g',
     def = 'retro',
-    domain = 'www.gravatar.com',
+    domain = process.env.NEXT_PUBLIC_GRAVATAR_HOST || 'www.gravatar.com',
     email,
     md5,
     name,
