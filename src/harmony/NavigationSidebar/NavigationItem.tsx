76dcf62370d015c92f73b50c940379c697b42eaf
Anton Sabotovich <sab94a@yandex.ru>
2024-08-14 13:19:29 +0300

    fix: add forwardRef

--- src/harmony/NavigationSidebar/NavigationItem.tsx
+++ src/harmony/NavigationSidebar/NavigationItem.tsx
@@ -2,32 +2,29 @@ import React, { FC, HTMLAttributes } from 'react';
 import cn from 'classnames';
 
 import { ListViewItem } from '../ListView/ListView';
-import { Link } from '../Link/Link';
 import { MenuItem } from '../MenuItem/MenuItem';
 
 import s from './NavigationSidebar.module.css';
 
 interface NavigationItemProps extends HTMLAttributes<HTMLDivElement> {
-    href: string;
+    value: string;
     selected?: boolean;
 }
 
-export const NavigationItem: FC<NavigationItemProps> = ({ selected, href, children, ...rest }) => (
+export const NavigationItem: FC<NavigationItemProps> = ({ selected, value, children, ...rest }) => (
     <ListViewItem
-        value={href}
+        value={value}
         renderItem={({ hovered, active, ...props }) => (
-            <Link view="primary" className={s.NavigationItemLink}>
-                <MenuItem
-                    hovered={active || hovered}
-                    className={cn({
-                        [s.MenuItem_active]: selected,
-                    })}
-                    {...props}
-                    {...rest}
-                >
-                    {children}
-                </MenuItem>
-            </Link>
+            <MenuItem
+                hovered={active || hovered}
+                className={cn({
+                    [s.MenuItem_active]: selected,
+                })}
+                {...props}
+                {...rest}
+            >
+                {children}
+            </MenuItem>
         )}
     />
 );
